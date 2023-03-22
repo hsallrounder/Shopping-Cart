d7112e01169145a9ad47852eae09dc68bba13d34
hsallrounder <hsallrounder@gmail.com>
2023-03-22 14:48:21 +0530

    yo

--- ecommerce-v2/auth-demo/index.js
+++ ecommerce-v2/auth-demo/index.js
@@ -4,47 +4,75 @@ const path = require("path")
 const mongoose = require("mongoose");
 const User = require("./models/User")
 const bcrypt = require('bcrypt');
+const session = require("express-session")
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+mongoose.connect("mongodb://127.0.0.1:27017/authDB")
+    .then(() => console.log("DB CONNECTED"))
+    .catch((err) => console.log(err));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 
+app.use(express.urlencoded({ extended: true }))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store : new MongoDBStore({
+        uri: 'mongodb://127.0.0.1:27017/authDB',
+        collection: 'mySessions'
+      }),
+    cookie: {
+        httpOnly:true,
+        maxAge: 7*24*60*60*1000
+    }
+}));
 
-app.use(express.urlencoded({extended:true}))
 
-mongoose.connect("mongodb://127.0.0.1:27017/authDB")
-.then(()=> console.log("DB CONNECTED"))
-.catch((err)=> console.log(err))
+let requireLogin = (req, res, next) => {
+
+    if (!req.session.user_id) {
+
+        return res.redirect("/login")
+    }
+
+
+    next();
+
+}
 
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
 
     res.send("home route")
 
 })
 
-app.get("/register",(req,res)=>{
+app.get("/register", (req, res) => {
 
     res.render("signup");
 
 
 })
 
-app.post("/register", async(req,res)=>{
+app.post("/register", async (req, res) => {
 
-    const {username, password} = req.body;
+    const { username, password } = req.body;
 
     const salt = await bcrypt.genSalt(12);
 
     const hash = await bcrypt.hash(password, salt);
 
-    await User.create({username , hash});
+    await User.create({ username, hash });
 
     res.redirect("/login")
 
 
 })
 
-app.get("/login", (req,res)=>{
+app.get("/login", (req, res) => {
 
     res.render("login")
 
@@ -52,27 +80,51 @@ app.get("/login", (req,res)=>{
 })
 
 
-app.post("/login", async(req,res)=>{
+app.post("/login", async (req, res) => {
 
-   const {username,password} = req.body;
+    const { username, password } = req.body;
 
-   const foundUser = await User.find({username});
+    const foundUser = await User.findOne({ username });
 
-   console.log(foundUser)
+    console.log(foundUser)
 
-   if(!foundUser){
+    if (!foundUser) {
 
-     return res.redirect("/register")
+        return res.redirect("/register")
 
-   }
+    }
+
+    const validUser = await bcrypt.compare(password, foundUser.hash);
+
+    if (!validUser) {
+
+        return res.send("inncorrect password entered")
+
+    }
+
+    req.session.user_id = foundUser._id
 
     return res.redirect("/dashboard")
 
 })
 
-app.get("/dashboard",(req,res)=>{
 
-    res.send(" you have successfully entered dashboard")
+app.get("/logout", (req, res) => {
+
+    req.session.destroy();
+
+    res.send("please login first")
+
+
+
+
+})
+
+app.get("/dashboard", requireLogin, (req, res) => {
+
+
+    res.send(" you have successfully entered dashboard");
+
 })
 
 
@@ -81,11 +133,10 @@ app.get("/dashboard",(req,res)=>{
 
 
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
 
     console.log("server running")
 
 
 
 })
-
